2a2321f6a4a5ba58bc1cd2c3a6c752d2e47bff35
soheib <sohaibyarian87@gmail.com>
2026-06-25 16:29:29 +0330

    add bg disabled

--- java/app.js
+++ java/app.js
@@ -3,6 +3,8 @@ const menu_icon = document.getElementById("menu_icon");
 const close_icon = document.getElementById("close_icon");
 const mobile_nav = document.getElementById("mobile_nav");
 
+const bgDisabled = document.getElementById("bg_disabled");
+
 window.addEventListener('scroll', () => {
   header.classList.toggle('scrolled', window.scrollY > 20);
 });
@@ -10,27 +12,44 @@ window.addEventListener('scroll', () => {
 function openMobileNav(){
   mobile_nav.style.transform = "translateY(0)";
   mobile_nav.style.opacity = 1;
+  header.classList.add("scrolled");
 }
 function closeMobileNav(){
   mobile_nav.style.transform = "translateY(-100vh)";
   mobile_nav.style.opacity = 0;
+  header.classList.remove("scrolled");
 }
 
-function toggleMenuIcons() {
-  const isMenuVisible = getComputedStyle(menu_icon).display == "block";
+function openBgDisabled(){
+  bgDisabled.style.display = "block";
+  bgDisabled.style.opacity = "1";
+  document.body.style.overflow = "hidden";
+}
+function closeBgDisabled(){
+  bgDisabled.style.display = "none";
+  bgDisabled.style.opacity = "0";
+  document.body.style.overflow = "auto";
+}
 
-  if (isMenuVisible) {
+bgDisabled.addEventListener('click' , ()=> {
+  closeBgDisabled();
+  closeMobileNav();
+  toggleMenuIcons();
+});
+
+
+function toggleMenuIcons() {
+  if (getComputedStyle(menu_icon).display == "block") {
     menu_icon.style.display = "none";
     close_icon.style.display = "block";
     openMobileNav();
+    openBgDisabled();
   }else{
     menu_icon.style.display = "block";
     close_icon.style.display = "none";
     closeMobileNav();
+    closeBgDisabled();
   }
-
-  // menu_icon.style.display = isMenuVisible ? "none" : "block";
-  // close_icon.style.display = isMenuVisible ? "block" : "none";
-};
+}
 menu_icon.addEventListener('click', toggleMenuIcons);
 close_icon.addEventListener('click', toggleMenuIcons);
