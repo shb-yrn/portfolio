215d2858ac43401405102b71c912383a4ba54de5
soheib <sohaibyarian87@gmail.com>
2026-07-01 21:43:28 +0330

    fix loading animation

--- java/app.js
+++ java/app.js
@@ -241,10 +241,12 @@ const mobileNavLinks = document.querySelectorAll('#mobile_nav li a')
 const bgDisabled = document.getElementById("bg_disabled");
 
 window.addEventListener("load" , ()=> {
-  loading.style.transform = "translateY(-100vh)";
   setTimeout(() => {
-    loading.style.display = "none";
-  }, 550);
+    loading.style.transform = "translateY(-100vh)";
+    setTimeout(() => {
+      loading.style.display = "none";
+    }, 550);
+  }, 500);
 });
 
 window.addEventListener('scroll', () => {
